8d0f4be226476f162f89f6b6e5bd1b89ef2da77d
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-11 07:50:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,3 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }// Your code here
-let dodger = document.getElementById("dodger");
-function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
-  }
-}
