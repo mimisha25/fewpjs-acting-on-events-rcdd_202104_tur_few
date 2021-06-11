b97e7e6df24178df62747588b288f02c12dd933d
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-11 07:53:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}// Your code here
+function moveDodgerRight(){
+   let leftNumbers = dodger.style.left.replace("px", "");
+   let left = parseInt(leftNumbers, 10);
+
+   if (left < 360) {
+     dodger.style.left = `${left + 1}px`;
+   }
+ }
